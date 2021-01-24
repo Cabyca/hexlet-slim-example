d953416c6aa642c2dc9b969a74d012aec67b0eaa
Alex <cabyca@yandex.ru>
2021-01-24 21:47:53 +0300

    Example post request

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+use function Differ\Formatters\Json\render;
+
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $container = new Container();
@@ -21,15 +23,12 @@ $app->get('/', function ($request, $response) {
 });
 
 $app->get('/users', function ($request, $response) use ($users) {
-
     $term = $request->getQueryParam('term');
-
     foreach ($users as $user) {
         if (strpos($user, $term) !== false) {
             $resultUsers[] = $user;
         }
     }
-
     $params = ['users' => $users, 'resultUsers' => $resultUsers, 'term' => $term];
     return $this->get('renderer')->render($response, "users/index.phtml", $params);
 });
@@ -39,6 +38,30 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
     return $response->write("Course id: {$id}");
 });
 
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['nickname' => '', 'email' => '', 'id' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
+$app->post('/users', function ($request, $response) {
+
+    $user = $request->getParsedBodyParam('user');
+
+    if (!file_exists('dataFile')) {
+        $dataJson = json_encode(["1" => $user]);
+        file_put_contents('dataFile', $dataJson);
+    } else {
+        $dataResult = json_decode(file_get_contents('dataFile'), true);
+        $dataResult[count($dataResult) + 1] = $user;
+        file_put_contents('dataFile', json_encode($dataResult));
+    }
+
+    return $response->withRedirect('/users', 302);
+});
+
 $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
@@ -46,43 +69,5 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     // $this в Slim это контейнер зависимостей
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
-
-$app->get('/users/new', function ($request, $response) {
-
-    $user = $request->getParsedBodyParam('user');
-
-    var_dump($user);
-
-    $params = ['user' => $user];
-
-    //$params = [
-    //    'user' => ['nickname' => '', 'email' => '', 'id' => ''],
-    //];
-    return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
-
-//$repo = new App\UserRepository();
-
-//$app->post('/users', function ($request, $response) use ($repo) {
-//$validator = new Validator();
-//    $user = $request->getParsedBodyParam('user');
-//$errors = $validator->validate($user);
-//if (count($errors) === 0) {
-//    $repo->save($user);
-//        return $response->withRedirect('/users', 302);
-//    }
-//    $params = [
-//        'user' => $user,
-//        'errors' => $errors
-//    ];
-//    $this->get('renderer')->render($response, "users/new.phtml", $params);
-//});
-//'/users', 
-
-
 
 $app->run();
