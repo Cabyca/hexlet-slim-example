1e5b49a5c535e5ab354ef72bb474a93ece69b439
Alex <cabyca@yandex.ru>
2021-02-01 20:19:38 +0300

    Add names routers

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
-use function Differ\Formatters\Json\render;
-
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $container = new Container();
@@ -17,12 +15,14 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($request, $response) use ($router) {
     $response->write('Welcome to Slim!');
     return $response;
-});
+})->setName('/');
 
-$app->get('/users', function ($request, $response) use ($users) {
+$app->get('/users', function ($request, $response) use ($users, $router) {
     $term = $request->getQueryParam('term');
     foreach ($users as $user) {
         if (strpos($user, $term) !== false) {
@@ -31,12 +31,12 @@ $app->get('/users', function ($request, $response) use ($users) {
     }
     $params = ['users' => $users, 'resultUsers' => $resultUsers, 'term' => $term];
     return $this->get('renderer')->render($response, "users/index.phtml", $params);
-});
+})->setName('users.index');
 
-$app->get('/courses/{id}', function ($request, $response, array $args) {
+$app->get('/courses/{id}', function ($request, $response, array $args) use ($router) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName('courses.show');
 
 $app->get('/users/new', function ($request, $response) {
     $params = [
@@ -44,9 +44,9 @@ $app->get('/users/new', function ($request, $response) {
         'errors' => []
     ];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
+})->setName('users.new');
 
-$app->post('/users', function ($request, $response) {
+$app->post('/users', function ($request, $response) use ($router) {
 
     $user = $request->getParsedBodyParam('user');
 
@@ -60,14 +60,14 @@ $app->post('/users', function ($request, $response) {
     }
 
     return $response->withRedirect('/users', 302);
-});
+})->setName('users.store');
 
-$app->get('/users/{id}', function ($request, $response, $args) {
+$app->get('/users/{id}', function ($request, $response, $args) use ($router) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
     // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
     // $this в Slim это контейнер зависимостей
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('users.show');
 
 $app->run();
